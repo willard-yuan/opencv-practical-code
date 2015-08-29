f44b7864679afb6ee33b6f7e1d42815fa4a111f1
Yong Yuan <yongyuanstu@gmail.com>
2015-08-29 21:42:58 +0800

    Update utils.h

--- geometric-verification-for-sift-match/utils.h
+++ geometric-verification-for-sift-match/utils.h
@@ -1,10 +1,20 @@
-#include <stdio.h>
+/*
+ * FileName : utils.c
+ * Author   : yongyuanstu@gmail.com
+ * Version  : v1.0
+ * Date     : 29 Aug 2015 08:31:41 PM CST
+ * Brief    : 
+ * 
+ * Copyright (C) MICL,USTB
+ */
+
+#include<iostream>
+#include<time.h>
+#include<windows.h>
 #include <vector>
-#include <iostream>
 
 #include "opencv/cv.h"
 #include "opencv/highgui.h"
-
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/features2d/features2d.hpp"
@@ -12,8 +22,29 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/nonfree/nonfree.hpp"
 
-using namespace std;
-using namespace cv;
+#include "armadillo"
 
-void drawMatch(const string &src, const string &obj, vector<Point2f> &srcPoints, vector<Point2f> &dstPoints);
-void findInliers(vector<KeyPoint> &qKeypoints, vector<KeyPoint> &objKeypoints, vector<DMatch> &matches, const string &imgfn, const string &objFileName);
+using namespace std;
+//using namespace cv;
+
+struct superluOpts{
+	int             tolerance1;  // default: true
+	int             tolerance2;    // default: false
+	int             tolerance3; // default: 1.0
+	unsigned int    minInliers;
+	unsigned int    numRefinementIterations;
+};
+
+/*********************生成随机颜色*****************/
+static cv::Scalar randomColor(cv::RNG& rng);
+
+/********************画匹配点**********************/
+void plotMatches(const cv::Mat &src, const cv::Mat &obj, vector<cv::Point2f> &srcPoints, vector<cv::Point2f> &dstPoints);
+
+/**************使用OpenCV自带的寻找内点************/
+void findInliers(vector<cv::KeyPoint> &qKeypoints, vector<cv::KeyPoint> &objKeypoints, vector<cv::DMatch> &matches, const string &imgfn, const string &objFileName);
+
+/******************自己写的寻找内点*****************/
+arma::mat centering(arma::mat &x);
+arma::mat toAffinity(arma::mat &f);
+arma::uvec geometricVerification(const arma::mat &frames1, const arma::mat &frames2, const arma::mat &matches, const superluOpts &opts);
